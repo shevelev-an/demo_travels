876054277d883b18c8a34552a4d7f099f4d233c5
ShanWebProf <Shev-an@yandex.ru>
2020-08-23 20:26:42 +0700

    commit 2: add files

---
+++ babel.config.js
@@ -0,0 +1,19 @@
+const presets = [
+  [
+    "@babel/env",
+    {
+      targets: {
+        edge: "15",
+        ie: "11",
+        firefox: "60",
+        chrome: "64",
+        safari: "11.1",
+        "esmodules": true
+      },
+      useBuiltIns: "usage",
+      corejs: "3.0.0"
+    }
+  ],
+];
+
+module.exports = { presets };
